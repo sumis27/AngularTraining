11d55daf2f892b2443e9c841c77b2a7e1ceda6e8
sumi-s <sumi.s@experionglobal.com>
2019-10-01 18:10:39 +0530

    Commit Check

--- myfirstapp/src/app/app.module.ts
+++ myfirstapp/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { wWarningAlertComponent } from './warning-alert/warning-alert.component';
+import { AssignmentComponent } from './assignment/assignment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    wWarningAlertComponent,
+    SuccessAlertComponent,
+    AssignmentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
